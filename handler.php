ab9a0fed59c7862412eebdf0f9fbc69d98a0b4fa
Igor2693 <i.bugaytsov@mail.ru>
2024-07-25 19:34:05 +0300

    fix: send email

--- handler.php
+++ handler.php
@@ -1,6 +1,7 @@
 <?php
 
 $user_phone = htmlspecialchars($_POST['userphone']);
+$user_email = htmlspecialchars($_POST['useremail']);
 
 
 $token = "7213367169:AAEkKxmGSjjgE6Gf3swGm9Xios7cohw52YU";
@@ -9,6 +10,10 @@ $chat_id = "-4214926859";
 $formData = array(
   "Телефон: " => $user_phone
 );
+$formData = array(
+  "Email: " => $user_email
+);
+
 
 foreach($formData as $key => $value) {
   $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A" ;
